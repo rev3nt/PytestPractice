8ddcc3afc9f0b13173fb8432fa76ffef6b49fc44
rev3nt <mikhial.merkulov.2005@mail.ru>
2025-10-18 18:40:23 +0300

    Module fixture added

--- tests/test_2.py
+++ tests/test_2.py
@@ -1,19 +1,8 @@
-import pytest
-
-
-@pytest.fixture
-def setup():
-    print("setup")
-    # yield также позволяет не только выполнять действия после теста, но и возвращает значение
-    yield 52
-    print("teardown")
-
-
 # Просто тесты, которые выполнятся без каких-либо дополнительных действий до или после
 def test_email(setup):
     print(f"test_email, and value: {setup}")
 
 
 # Просто тесты, которые выполнятся без каких-либо дополнительных действий до или после
-def test_login():
+def test_login(success_message):
     print("test_login")

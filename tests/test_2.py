cac522578b3854844aa0c1da6afc630838fae0da
rev3nt <mikhial.merkulov.2005@mail.ru>
2025-10-18 18:29:01 +0300

    yield added

--- tests/test_2.py
+++ tests/test_2.py
@@ -1,6 +1,17 @@
+import pytest
+
+
+@pytest.fixture
+def setup():
+    print("setup")
+    # yield также позволяет не только выполнять действия после теста, но и возвращает значение
+    yield 52
+    print("teardown")
+
+
 # Просто тесты, которые выполнятся без каких-либо дополнительных действий до или после
-def test_email():
-    print("test_email")
+def test_email(setup):
+    print(f"test_email, and value: {setup}")
 
 
 # Просто тесты, которые выполнятся без каких-либо дополнительных действий до или после
